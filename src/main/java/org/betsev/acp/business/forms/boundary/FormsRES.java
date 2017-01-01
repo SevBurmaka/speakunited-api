025615940a90d613f8ffbd8babf983ca3036c1bb
sev <sev.burmaka@hart.com>
2017-01-01 15:06:17 -0800

    added Forms forwarding rest endpoint

---
+++ src/main/java/org/betsev/acp/business/forms/boundary/FormsRES.java
@@ -0,0 +1,44 @@
+package org.betsev.acp.business.forms.boundary;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+/**
+ * Created by sevburmaka on 1/1/17.
+ */
+@RestController
+@RequestMapping("/forms")
+public class FormsRES {
+    public static final String EXTERNAL_FORM_API = "https://congressforms.eff.org";
+    public static final String RETRIEVE_FORM_ELEMENTS = "/retrieve-form-elements";
+    public static final String FILL_OUT_FORM = "/fill-out-form";
+    public static final String FILL_OUT_CAPTCHA = "/fill-out-captcha";
+
+
+    @RequestMapping(value = RETRIEVE_FORM_ELEMENTS,method = RequestMethod.POST)
+    public HashMap retrieveFormElements(@RequestBody HashMap data){
+        return forwardRequest(data,EXTERNAL_FORM_API+RETRIEVE_FORM_ELEMENTS);
+    }
+
+    @RequestMapping(value = FILL_OUT_FORM,method = RequestMethod.POST)
+    public HashMap fillOutForm(@RequestBody HashMap data){
+        return forwardRequest(data,EXTERNAL_FORM_API+FILL_OUT_FORM);
+    }
+
+    @RequestMapping(value = FILL_OUT_CAPTCHA,method = RequestMethod.POST)
+    public HashMap fillOutCaptcha(@RequestBody HashMap data){
+        return forwardRequest(data,EXTERNAL_FORM_API+FILL_OUT_CAPTCHA);
+    }
+
+    private HashMap forwardRequest(HashMap data, String url){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForObject(url,data,HashMap.class);
+    }
+
+
+}
