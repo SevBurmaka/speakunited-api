ca5ffbe107ebf56f963f5fa306f76de2cc9dfd0e
sev <sev.burmaka@hart.com>
2017-01-29 15:41:43 -0800

    updating scrip fields and issues file

--- src/main/java/org/betsev/acp/business/issue/entity/Script.java
+++ src/main/java/org/betsev/acp/business/issue/entity/Script.java
@@ -1,14 +1,20 @@
 package org.betsev.acp.business.issue.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 /**
  * Created by sevburmaka on 12/11/16.
  */
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Script {
     String type;
     String text;
+    String title;
+    List<String> hashTags;
 }
