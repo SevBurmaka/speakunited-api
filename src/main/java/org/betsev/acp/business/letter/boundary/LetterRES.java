539645701b337ffd10a91c0a3069084d6c053d7b
sev <sev.burmaka@hart.com>
2017-01-29 16:18:36 -0800

    letter service impl and rest endpoint

---
+++ src/main/java/org/betsev/acp/business/letter/boundary/LetterRES.java
@@ -0,0 +1,33 @@
+package org.betsev.acp.business.letter.boundary;
+
+import org.betsev.acp.business.letter.entity.PaidLetterRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sevburmaka on 1/29/17.
+ */
+@RestController
+@RequestMapping("/api/letter")
+public class LetterRES {
+    private static final Logger LOG = LoggerFactory.getLogger(org.betsev.acp.business.letter.boundary.LetterRES.class);
+
+    @Autowired
+    LetterService letterService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<String> sendFax(@RequestBody PaidLetterRequest letterRequest){
+        LOG.info("Processing letter request: {}",letterRequest);
+        boolean success = letterService.sendLetter(letterRequest);
+        if (success)
+            return ResponseEntity.ok("Success");
+        else
+            return ResponseEntity.badRequest().body(null);
+    }
+}
--- src/main/java/org/betsev/acp/business/letter/boundary/LobLetterService.java
+++ src/main/java/org/betsev/acp/business/letter/boundary/LobLetterService.java
@@ -1,16 +1,28 @@
 package org.betsev.acp.business.letter.boundary;
 
+import com.lob.client.AsyncLobClient;
+import com.lob.client.LobClient;
+import com.lob.protocol.request.AddressRequest;
+import com.lob.protocol.response.LetterResponse;
 import org.betsev.acp.business.contact.boundary.ContactService;
+import org.betsev.acp.business.contact.entity.Address;
+import org.betsev.acp.business.contact.entity.Contact;
 import org.betsev.acp.business.letter.entity.LetterRequest;
 import org.betsev.acp.business.letter.entity.PaidLetterRequest;
 import org.betsev.acp.config.ApiKeyConfig;
+import org.betsev.acp.support.LetterUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.io.File;
 
 /**
  * Created by sevburmaka on 1/29/17.
  */
 public class LobLetterService implements LetterService {
+    private static final Logger LOG = LoggerFactory.getLogger(LobLetterService.class);
     @Autowired
     ApiKeyConfig apiKeyConfig;
 
@@ -23,8 +35,51 @@ public class LobLetterService implements LetterService {
         PaidLetterRequest paidLetterRequest = (PaidLetterRequest)letter;
         if (!doPayment(paidLetterRequest))
             return false;
+        Contact contact = contactService.getByBioguideId(letter.getBioguideId());
+        Address address;
+        //@todo need to have a field in the request to decide which of these we are contact
+        if (contact.getDcAddress() != null)
+            address = contact.getDcAddress();
+        else if (contact.getDistrictAddress() != null)
+            address = contact.getDistrictAddress();
+        else {
+            LOG.error("No addresses found for bioguide: {}",letter.getBioguideId());
+            return false;
+        }
 
+        final LobClient client = AsyncLobClient.createDefault("test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc");
+
+        File letterFile = LetterUtil.createHtmlFile(letter);
+        final com.lob.protocol.request.LetterRequest letterRequest = com.lob.protocol.request.LetterRequest.builder()
+                .to(createAddressRequest(contact.getName(),address))
+                .file(letterFile)
+                .color(true)
+                .build();
+
+        try {
+            final LetterResponse letterResponse = client.createLetter(letterRequest).get();
+            LOG.info("Generated letter: {}", letterResponse);
+        }
+        catch (final Exception e) {
+            LOG.error("Failed Letter {}", letter,e);
+            return false;
+        }
+        finally{
+            LetterUtil.deleteOnAfterDelay(letterFile);
+        }
         return true;
+    }
+
+    private AddressRequest createAddressRequest(String repName, Address address){
+        return AddressRequest.builder()
+                .name(repName)
+                .line1(address.getLine1())
+                .line2(address.getLine2())
+                .city(address.getCity())
+                .state(address.getState())
+                .zip(address.getZip())
+                .country("US")
+                .build();
     }
 
     public boolean doPayment(PaidLetterRequest request){
