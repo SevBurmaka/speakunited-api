d18cd807fec42ffc862cceaca7f2dd5896e0cf87
sev <sev.burmaka@hart.com>
2017-01-29 16:19:56 -0800

    changed hashtag format

--- src/main/java/org/betsev/acp/business/issue/entity/Script.java
+++ src/main/java/org/betsev/acp/business/issue/entity/Script.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 /**
  * Created by sevburmaka on 12/11/16.
  */
@@ -16,5 +14,5 @@ public class Script {
     String type;
     String text;
     String title;
-    List<String> hashTags;
+    String hashTags;
 }
